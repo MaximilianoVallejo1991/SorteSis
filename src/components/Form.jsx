94c7fd5e4df33e1f5faaf4ed18409c6ee0ab43b2
Deimos <jmaximilianovallejo@gmail.com>
2025-04-23 10:11:31 -0300

    funcion de enter en sorteo random

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -18,31 +18,60 @@ function Form({ setNames, setPrizes, handleRaffle }) {
     handleRaffle();
   };
 
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      addName();
+    }
+  };
+
   return (
-    <div className="form-container">
-      <h2>Agregar Participantes</h2>
-      <div className="form-group">
-        <input 
-          type="text" 
-          value={currentName} 
-          onChange={(e) => setCurrentName(e.target.value)} 
-          placeholder="Nombre del participante" 
-        />
-        <button className="add-form-button" onClick={addName}>Agregar</button>
+
+    <div className="info-container">
+
+      <div className="form-container">
+
+        <h2>Agregar Participantes</h2>
+        <div className="form-group">
+          <input
+            type="text"
+            value={currentName}
+            onChange={(e) => setCurrentName(e.target.value)}
+            onKeyDown={handleKeyPress}
+            placeholder="Nombre del participante"
+          />
+          <button className="add-form-button" onClick={addName}>Agregar</button>
+        </div>
+        <div className="form-group">
+          <label>Cantidad de Premios:</label>
+          <input
+            type="number"
+            value={currentPrizes}
+            onChange={(e) => setCurrentPrizes(Number(e.target.value))}
+            min="1"
+          />
+        </div>
+        <button className="start-button" onClick={startRaffle}>
+          Realizar Sorteo
+        </button>
+
       </div>
-      <div className="form-group">
-        <label>Cantidad de Premios:</label>
-        <input 
-          type="number" 
-          value={currentPrizes} 
-          onChange={(e) => setCurrentPrizes(Number(e.target.value))} 
-          min="1" 
-        />
+
+      <div className="participant-list">
+        <h2>Lista de Participantes</h2>
+        <ul className="grid-container">
+          {participantList.map((name, index) => (
+            <li key={index}>{name}</li>
+          ))}
+        </ul>
+        {participantList.length === 0 && <p>No hay participantes.</p>}
+
+
       </div>
-      <button className="start-button" onClick={startRaffle}>
-        Realizar Sorteo
-      </button>
+
     </div>
+
+
+
   );
 }
 
